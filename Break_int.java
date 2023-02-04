479c68d022bdbab8a06fc7a9bf7ab308bb5e2d50
codingwithmegha <122961155+codingwithmegha@users.noreply.github.com>
2023-02-04 13:20:33 +0530

    Add files via upload

---
+++ Break_int.java
@@ -0,0 +1,49 @@
+package Object_class;
+import java.util.Scanner;
+class Break_int {
+public static void main(String[] args) {
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the integer");
+int a=sc.nextInt();
+//convert in integer
+String b=Integer.toString(a);
+//convert in char array
+char[]s=b.toCharArray();
+System.out.println("the individual char are:");
+for(int i=0; i<s.length; i++)
+{
+	System.out.print(s[i]+" ");
+}
+}
+	
+
+		}
+
+
+
+	
+		
+
+
+		
+
+		
+		
+
+
+
+		
+		
+				
+
+		
+
+		
+
+	
+			
+
+
+		
+	
+	
